54a5e6a032c0d51064ff28ce72b7765c63925f45
josesoyo <jose.gonzalez@for.unipi.it>
2015-09-02 14:58:18 +0200

    Major bug solved on refr/refl with meshes due to the misuse of ray.lenght.  Script Test_Gourd.fsx added too

--- Test_Gourd.fsx
+++ Test_Gourd.fsx
@@ -26,8 +26,8 @@ open RayColor
 //Definitions of values
 //
 // Pixels
-let PixNumW = 30
-let PixNumH = 30
+let PixNumW = 500
+let PixNumH = 500
 let PixWide = 2.0/float(PixNumW) //Write something
 let PixHeigh = 2.0/float(PixNumH)
 //
@@ -35,17 +35,17 @@ let PixHeigh = 2.0/float(PixNumH)
 //let sensor = Sensor(PixWide, PixHeigh, PixNumW, PixNumH) 
  
 //Scenario1
-let camera={EyePoint=Point3D(6.50,-6.50, 6.75);LookAt=Vector3D(-2.0,2.0,-2.0); Up=Vector3D(0.0,0.0,1.0)}  
+let camera={EyePoint=Point3D(6.50,-6.50, 6.75);LookAt=Vector3D(-2.20,2.20,-2.20); Up=Vector3D(0.0,0.0,1.0)}  
 
-let light = {origin =  Point3D(10.0,10.0,35.0);color=Color(1.0,0.0,0.0); intensity = 3000.0}
-let light1 = {origin = Point3D(0.0, -10.,30.0);color=Color(0.0,0.0,1.0); intensity = 3000.0}
-let light2 = {origin = Point3D(-10.0,5.0,25.5);color=Color(0.0,1.0,0.0); intensity = 3000.0}
+let light = {origin =  Point3D(10.0,10.0,25.0);color=Color(1.0,1.0,1.0); intensity = 3000.0}
+let light1 = {origin = Point3D(-5.0, -10.,25.0);color=Color(1.0,1.0,1.0); intensity = 3000.0}
+let light2 = {origin = Point3D(-10.0,10.0,25.5);color=Color(1.0,1.0,1.0); intensity = 3000.0}
 
 
 //
 // scene
 // Materials
-let refractive= {DiffuseLight = Color(0.005,0.005,0.0175);SpecularLight = Color(0.45,0.45,0.59);shinness= 100; R=0.01; T=0.99; n= 1.45;Fresnel=true} 
+let refractive= {DiffuseLight = Color(0.005,0.005,0.0175);SpecularLight = Color(0.4,0.4,0.5);shinness= 100; R=0.01; T=0.99; n= 1.45;Fresnel=true} 
 //let reflective ={DiffuseLight = Color(0.25,0.490,0.25);SpecularLight = Color(0.9,0.9,0.9);shinness= 50; R=0.950; T=0.0; n= 1.45;Fresnel = false} 
 let whitte ={DiffuseLight = Color(0.9,0.90,0.9);SpecularLight = Color(0.51,0.51,0.51);shinness= 6; R=0.050; T=0.0; n= 1.45;Fresnel = false}
 // I define the spheres and meshes
@@ -89,7 +89,7 @@ for i in 0..(PixNumW-1) do
                      //(fun x -> x) |> |> List.head S
                     //printfn "Color is: %+A" color
                     bmp.SetPixel(i,j,Color.FromArgb(int(255.0*color.r),  int(255.0*color.g) , int(255.0*color.b) ))
-    if i%10=0 then printfn "%i" i
+    if i%20=0 then printfn "%i" i
 
 
 let form = new Form(Text="Rendering test",TopMost=true)
@@ -100,4 +100,3 @@ form.Controls.Add(img)
 
 img.Image <- bmp
 printfn "end"
-//bmp.Save(@"C:\Users\JoseM\Desktop\test_gourd.jpg")
